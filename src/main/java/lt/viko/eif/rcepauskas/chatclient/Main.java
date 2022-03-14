acb504d71b4b83485a142a93c7c89db7f69a771a
raimax <4miner44@gmail.com>
2022-03-14 11:33:53 +0200

    commit

--- src/main/java/lt/viko/eif/rcepauskas/chatclient/Main.java
+++ src/main/java/lt/viko/eif/rcepauskas/chatclient/Main.java
@@ -11,7 +11,7 @@ import java.net.Socket;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("chatWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(getClass().getResource("listStyles.css").toExternalForm());
         stage.setTitle("Chat client");
